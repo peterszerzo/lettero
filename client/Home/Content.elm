814badedf097c0a8868daf980f3dcf28b64a8b08
Peter Szerzo <szerzo.peter@gmail.com>
2016-10-09 18:54:12 +0200

    Tweak styles

--- client/Home/Content.elm
+++ client/Home/Content.elm
@@ -8,5 +8,5 @@ Oh, words. So complex and full of surprises. We can hardly make head or tail of 
 
 Lettero is a tiny little game allowing players to compete against each other discovering words that are displayed in a less conformist fashion. In a circle. Looping around. Scattered all over.
 
-Enjoy :).
+You go enjoy now 😊 !
 """
