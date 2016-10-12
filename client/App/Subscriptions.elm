631610284d0a6339f312f4697e7500b68071ea55
Peter Szerzo <szerzo.peter@gmail.com>
2016-10-12 07:28:46 +0200

    Forward subscriptions

--- client/App/Subscriptions.elm
+++ client/App/Subscriptions.elm
@@ -1,8 +1,14 @@
 module Subscriptions exposing (..)
 
 import Models exposing (Model)
-import Messages exposing (Msg)
+import Messages exposing (Msg(..))
+import Game.Subscriptions exposing (subscriptions)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  case model.game of
+    Just game ->
+      Sub.map GameMsg (Game.Subscriptions.subscriptions game)
+
+    Nothing ->
+      Sub.none
