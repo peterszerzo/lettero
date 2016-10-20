fdba6c5ac5cebe947524071233af9870219b5ef7
Peter Szerzo <peter.szerzo@airtame.com>
2016-10-20 13:05:14 -0400

    Add firebase

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import domReady from 'domready';
+import attachFastClick from 'fastclick';
+import firebase from 'firebase';
+
+import './styles/index.css';
+import './index.html';
+import './assets/favicon.ico';
+
+import Elm from './App/Main.elm';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+domReady(() => {
+  attachFastClick.attach(document.body);
+  const app = firebase.initializeApp(config);
+  console.log(app, Elm);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,19 @@ const webpack = require('webpack');
 const validate = require('webpack-validator');
 const postCssCssNext = require('postcss-cssnext');
 const postCssImport = require('postcss-import');
+const dotenv = require('dotenv');
 
-const commonPlugins = [];
+dotenv.load();
+
+const commonPlugins = [
+  new webpack.DefinePlugin({
+    'process.env.FIREBASE_API_KEY': `"${process.env.FIREBASE_API_KEY}"`,
+    'process.env.FIREBASE_AUTH_DOMAIN': `"${process.env.FIREBASE_AUTH_DOMAIN}"`,
+    'process.env.FIREBASE_DATABASE_URL': `"${process.env.FIREBASE_DATABASE_URL}"`,
+    'process.env.FIREBASE_STORAGE_BUCKET': `"${process.env.FIREBASE_STORAGE_BUCKET}"`,
+    'process.env.FIREBASE_MESSAGING_SENDER_ID': `"${process.env.FIREBASE_MESSAGING_SENDER_ID}"`
+  })
+];
 
 const prodPlugins = [
   new webpack.optimize.UglifyJsPlugin({})
@@ -14,10 +25,10 @@ const prodPlugins = [
 
 const config = {
   entry: [
-    path.join(__dirname, 'client/index.js')
+    path.join(__dirname, 'src/index.js')
   ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'build'),
     publicPath: '/',
     filename: 'index.js'
   },
@@ -25,7 +36,8 @@ const config = {
     loaders: [
       {
         test: /\.js/,
-        loader: 'babel'
+        loader: 'babel',
+        exclude: /node_modules/
       },
       {
         test: /\.css/,
@@ -36,7 +48,7 @@ const config = {
         loader: 'elm-webpack'
       },
       {
-        test: /\.ico$/,
+        test: /\.(ico|html)$/,
         loader: 'file?name=[name].[ext]'
       },
       {
