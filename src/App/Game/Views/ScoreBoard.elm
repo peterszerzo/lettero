35ec0c3011ab99278d1276390f597e1e081b4e2e
Peter Szerzo <szerzo.peter@gmail.com>
2016-11-06 14:03:40 +0100

    Styled score board

--- src/App/Game/Views/ScoreBoard.elm
+++ src/App/Game/Views/ScoreBoard.elm
@@ -1,29 +1,34 @@
 module Game.Views.ScoreBoard exposing (view)
 
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, p, span, text)
 import Html.Attributes exposing (style, class, classList)
 
 import Models.Player as Player
 import Game.Messages exposing (Msg(..))
 
-viewPlayer : Player.Player -> Html Msg
-viewPlayer { id, score } =
-  div
+viewPlayer : Int -> Player.Player -> Html Msg
+viewPlayer maxScore { id, score } =
+  p
     [ class "score-board__item"
     ]
-    [ p [ class "score-board__player" ] [ text id ]
-    , p [ class "score-board__score" ] [ text (toString score) ]
+    [ span [ class "score-board__player" ] [ text id ]
+    , span [ class "score-board__score" ] [ text (toString score) ]
+    , span [ class "score-board__mark" ] [ text (if score == maxScore then "♛" else "") ]
     ]
 
 view : String -> Player.Players -> Html Msg
 view playerId players =
-  div
-    [ class "score-board"
-    ]
-    [ div [ class "score-board__items" ]
-        (
-          players
-            |> Player.toList
-            |> List.map viewPlayer
-        )
-    ]
+  let
+    playersList = players |> Player.toList
+    scores : List Int
+    scores = playersList |> List.map .score
+  in
+    div
+      [ class "score-board"
+      ]
+      [ div [ class "score-board__items" ]
+          (
+            playersList
+              |> List.map (viewPlayer (List.maximum scores |> Maybe.withDefault 0))
+          )
+      ]
