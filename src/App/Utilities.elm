53cbb11c96daeadee11ee0edba581efc5ce4d661
Peter Szerzo <peter.szerzo@airtame.com>
2016-12-07 08:53:27 +0100

    Add isAllLowercaseLetter helper

--- src/App/Utilities.elm
+++ src/App/Utilities.elm
@@ -5,8 +5,13 @@ import Regex
 
 textTemplate : String -> String -> String
 textTemplate template value =
-  Regex.replace
-    Regex.All
-    ("${}" |> Regex.escape |> Regex.regex)
-    (always value)
-    template
+    Regex.replace
+        Regex.All
+        ("${}" |> Regex.escape |> Regex.regex)
+        (always value)
+        template
+
+
+isAllLowercaseLetter : String -> Bool
+isAllLowercaseLetter =
+    (==) 1 << List.length << Regex.find Regex.All (Regex.regex "^[a-z]*$")
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -8,9 +8,17 @@ import Utilities
 all : Test
 all =
     describe "Lettero"
-        [ describe "Utilities"
-            [ test "textTemplate" <|
+        [ describe "Utilities.textTemplate"
+            [ test "replaces ${} with argument string" <|
                 \() ->
                     Expect.equal (Utilities.textTemplate "Hello, ${}!" "Lettero") "Hello, Lettero!"
+            ]
+        , describe "Utilities.isAllLowercaseLetter"
+            [ test "returns true if all lowercase letter" <|
+                \() ->
+                    Expect.equal (Utilities.isAllLowercaseLetter "adsf") True
+            , test "returns false otherwise" <|
+                \() ->
+                    Expect.equal (Utilities.isAllLowercaseLetter "adsf1") False
             ]
         ]
