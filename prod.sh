9c89ad6acbbbf353eacc23dc558fb392617e8fa6
peterszerzo <szerzo.peter@gmail.com>
2017-01-05 13:44:39 +0100

    Add tests, precommit hooks. Cleaned up build

--- prod.sh
+++ prod.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 postcss ./src/index.css --use postcss-cssnext --output ./src/index.compiled.css &&
-elm-app build &&
-sed -i -e 's/js\/main/\/js\/main/g' ./dist/index.html &&
-sed -i -e 's/css\/main/\/css\/main/g' ./dist/index.html
+elm-app build
